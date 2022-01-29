68ef0e320ff24df03f1da6619c1e719ca7a36142
Adriana Corona <adrcoron@amazon.com>
2022-01-29 13:20:32 -0500

    Add folder structure and sample drive train

---
+++ TestMotors/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class DriveTrain extends SubsystemBase {
+  /** Creates a new DriveTrain. */
+
+  private final PWMSparkMax motorLeft1 = new PWMSparkMax(Constants.MOTOR_LEFT_1_ID);
+  private final PWMSparkMax motorLeft2 = new PWMSparkMax(Constants.MOTOR_LEFT_2_ID);
+  private final PWMSparkMax motorRight1 = new PWMSparkMax(Constants.MOTOR_RIGHT_1_ID);
+  private final PWMSparkMax motorRight2 = new PWMSparkMax(Constants.MOTOR_RIGHT_2_ID);
+
+  private final MotorControllerGroup leftMotors = new MotorControllerGroup(motorLeft1, motorLeft2);
+  private final MotorControllerGroup rightMotors = new MotorControllerGroup(motorRight1, motorRight2);
+
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(
+      leftMotors, rightMotors);
+
+  public void ArcadeDrive(double speed, double rotation) {
+    m_robotDrive.arcadeDrive(speed, rotation);
+  }
+
+  public DriveTrain() {
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
