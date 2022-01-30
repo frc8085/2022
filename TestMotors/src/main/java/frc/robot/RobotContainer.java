205839c1250bd01cf0c482aa0492b825fe98f01b
Adriana Corona <adrcoron@amazon.com>
2022-01-30 18:49:49 -0500

    Start Shooter

--- TestMotors/src/main/java/frc/robot/RobotContainer.java
+++ TestMotors/src/main/java/frc/robot/RobotContainer.java
@@ -5,11 +5,16 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
 import frc.robot.commands.ArcadeDriver;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.Shooter;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
@@ -26,7 +31,9 @@ public class RobotContainer {
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   private final XboxController m_driverController = new XboxController(Constants.OIConstants.kDriverControllerPort);
+  private final XboxController m_operatorController = new XboxController(Constants.OIConstants.kOperatorControllerPort);
   private final DriveTrain m_robotDrive = new DriveTrain();
+  private final Shooter m_shooter = new Shooter();
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -47,6 +54,29 @@ public class RobotContainer {
    * Use this method to define your button->command mappings.
    */
   private void configureButtonBindings() {
+    // Spin up the shooter when the 'A' button is pressed
+    new JoystickButton(m_operatorController, Button.kA.value)
+        .whenPressed(new InstantCommand(m_shooter::enable, m_shooter));
+
+    // Turn off the shooter when the 'B' button is pressed
+    new JoystickButton(m_operatorController, Button.kB.value)
+        .whenPressed(new InstantCommand(m_shooter::disable, m_shooter));
+
+    // Run the feeder when the 'X' button is held, but only if the shooter is at
+    // speed
+    // new JoystickButton(m_operatorController, Button.kX.value)
+    // .whenPressed(
+    // new ConditionalCommand(
+    // // Run the feeder
+    // new InstantCommand(m_shooter::runFeeder, m_shooter),
+    // // Do nothing
+    // new InstantCommand(),
+    // // Determine which of the above to do based on whether the shooter has
+    // reached
+    // // the
+    // // desired speed
+    // m_shooter::atSetpoint))
+    // .whenReleased(new InstantCommand(m_shooter::stopFeeder, m_shooter));
   }
 
   /**
