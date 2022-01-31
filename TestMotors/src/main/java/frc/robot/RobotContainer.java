95d47051bf6a51b18e688232c9f883ce1516ae22
Adriana Corona <adrcoron@amazon.com>
2022-01-30 19:08:00 -0500

    Testing operator controller

--- TestMotors/src/main/java/frc/robot/RobotContainer.java
+++ TestMotors/src/main/java/frc/robot/RobotContainer.java
@@ -46,7 +46,7 @@ public class RobotContainer {
         // A split-stick arcade command, with forward/backward controlled by the left
         // hand, and turning controlled by the right.
         new ArcadeDriver(
-            m_robotDrive, m_driverController::getRightX, m_driverController::getLeftY));
+            m_robotDrive, m_driverController::getRightX, m_operatorController::getLeftY));
 
   }
 
