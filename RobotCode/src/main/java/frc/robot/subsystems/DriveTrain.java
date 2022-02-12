a2b32f75984656cd51eb1053ee011b95f94b0a7f
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2022-02-12 15:35:25 -0500

    Rate limit drive speed

--- RobotCode/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ RobotCode/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -7,37 +7,43 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 public class DriveTrain extends SubsystemBase {
-  /** Creates a new DriveTrain. */
+    /** Creates a new DriveTrain. */
 
-  // Left motors
-  private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(
-      new CANSparkMax(DriveConstants.kLeftMotor1Port,
-          MotorType.kBrushless),
-      new CANSparkMax(DriveConstants.kLeftMotor2Port,
-          MotorType.kBrushless));
+    // Left motors
+    private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(
+            new CANSparkMax(DriveConstants.kLeftMotor1Port,
+                    MotorType.kBrushless),
+            new CANSparkMax(DriveConstants.kLeftMotor2Port,
+                    MotorType.kBrushless));
 
-  // Right motors
-  private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(
-      new CANSparkMax(DriveConstants.kRightMotor1Port,
-          MotorType.kBrushless),
-      new CANSparkMax(DriveConstants.kRightMotor2Port,
-          MotorType.kBrushless));
+    // Right motors
+    private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(
+            new CANSparkMax(DriveConstants.kRightMotor1Port,
+                    MotorType.kBrushless),
+            new CANSparkMax(DriveConstants.kRightMotor2Port,
+                    MotorType.kBrushless));
 
-  // The robot's drive
-  private final DifferentialDrive m_drive = new DifferentialDrive(
-      m_leftMotors, m_rightMotors);
+    // The robot's drive
+    private final DifferentialDrive m_drive = new DifferentialDrive(
+            m_leftMotors, m_rightMotors);
 
-  // Drive the robot using ArcadeDrive command
-  public void ArcadeDrive(double speed, double rotation) {
-    m_drive.arcadeDrive(speed, rotation);
-  }
+    // Creates a SlewRateLimiter that limits the rate of change of the signal to 10%
+    // units per second
+    SlewRateLimiter driveFilter = new SlewRateLimiter(0.1);
 
-  public DriveTrain() {
-  }
+    // Drive the robot using ArcadeDrive command
+    public void ArcadeDrive(double speed, double rotation) {
+        // Maxes out speed at 75%
+        m_drive.arcadeDrive(driveFilter.calculate(speed * 0.75), rotation);
+    }
+
+    public DriveTrain() {
+    }
 }
