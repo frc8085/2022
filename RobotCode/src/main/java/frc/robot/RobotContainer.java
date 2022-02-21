048f999610f5fa092e06c150fc6450ff04c01337
Adriana Corona <adrcoron@amazon.com>
2022-02-21 12:00:30 -0500

    Clean up robot container

--- RobotCode/src/main/java/frc/robot/RobotContainer.java
+++ RobotCode/src/main/java/frc/robot/RobotContainer.java
@@ -47,157 +47,143 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTableEntry;
 
 public class RobotContainer {
-        // The robot's subsystems and commands
-        private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-        private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  // The robot's subsystems and commands
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
-        // Drive train and driver controller
-        private final XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
-        private final DriveTrain m_robotDrive = new DriveTrain();
+  // Drive train and driver controller
+  private final XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+  private final DriveTrain m_robotDrive = new DriveTrain();
 
-        // OPerator controller and subsystems
-        private final XboxController m_operatorController = new XboxController(
-                        OIConstants.kOperatorControllerPort);
-        private final Shooter m_shooter = new Shooter();
-        private final Feeder m_feeder = new Feeder();
-        private final Intake m_intake = new Intake();
-        private final Conveyor m_conveyor = new Conveyor();
-        private final Hatch m_hatch = new Hatch();
+  // OPerator controller and subsystems
+  private final XboxController m_operatorController = new XboxController(
+      OIConstants.kOperatorControllerPort);
+  private final Shooter m_shooter = new Shooter();
+  private final Feeder m_feeder = new Feeder();
+  private final Intake m_intake = new Intake();
+  private final Conveyor m_conveyor = new Conveyor();
+  private final Hatch m_hatch = new Hatch();
 
-        /**
-         * Shooting Mode definition
-         * shootMode is an integer corresponding to the three types of tragets
-         * (High Near, High Far, Low)
-         * shootingMode translates the integer into a string so we can display it
-         * in the Shuffleboard entry shottingModeDisplay
-         */
-        private NetworkTableEntry shootingModeDisplay;
-        private int shootMode = OIConstants.kShooterOff;
-        private static final Map<Integer, String> shootingMode = new HashMap<Integer, String>() {
-                {
-                        put(OIConstants.kShooterOff, "Shooting mode not selected");
-                        put(OIConstants.kTargetHighNear, "High->Near");
-                        put(OIConstants.kTargetHighFar, "High----------------------------->Far");
-                        put(OIConstants.kTargetLow, "Low");
-                }
-        };
+  /**
+   * Shooting Mode definition
+   * shootMode is an integer corresponding to the three types of tragets
+   * (High Near, High Far, Low)
+   * shootingMode translates the integer into a string so we can display it
+   * in the Shuffleboard entry shottingModeDisplay
+   */
+  private NetworkTableEntry shootingModeDisplay;
+  private int shootMode = OIConstants.kShooterOff;
+  private static final Map<Integer, String> shootingMode = new HashMap<Integer, String>() {
+    {
+      put(OIConstants.kShooterOff, "Shooting mode not selected");
+      put(OIConstants.kTargetHighNear, "High->Near");
+      put(OIConstants.kTargetHighFar, "High----------------------------->Far");
+      put(OIConstants.kTargetLow, "Low");
+    }
+  };
 
-        public RobotContainer() {
-                configureButtonBindings();
+  public RobotContainer() {
+    configureButtonBindings();
 
-                m_robotDrive.setDefaultCommand(
-                                // TODO: Joystick directions are fixed. Need to fix actual
-                                // Motors now
-                                // Forward/backward controlled by the left joystick
-                                // Turning controlled by the right joystick
-                                new ArcadeDriver(
-                                                m_robotDrive,
-                                                m_driverController::getLeftY,
-                                                m_driverController::getRightX));
+    m_robotDrive.setDefaultCommand(
+        // Forward/backward controlled by the left joystick
+        // Turning controlled by the right joystick
+        new ArcadeDriver(
+            m_robotDrive,
+            m_driverController::getLeftY,
+            m_driverController::getRightX));
 
-                shootingModeDisplay = Shuffleboard.getTab("Shooter")
-                                .add("Shooting Mode", shootingMode.get(shootMode))
-                                .withPosition(0, 0)
-                                .withSize(2, 1)
-                                .getEntry();
+    shootingModeDisplay = Shuffleboard.getTab("Shooter")
+        .add("Shooting Mode", shootingMode.get(shootMode))
+        .withPosition(0, 0)
+        .withSize(2, 1)
+        .getEntry();
 
-        }
+  }
 
-        private void configureButtonBindings() {
-                // Create some buttons
-                final JoystickButton shootButton = new JoystickButton(m_operatorController, Button.kRightBumper.value);
-                final JoystickButton shooterOffButton = new JoystickButton(m_operatorController, Button.kX.value);
-                final JoystickButton setTargetHighNearButton = new JoystickButton(m_operatorController,
-                                Button.kY.value);
-                final JoystickButton setTargetHighFarButton = new JoystickButton(m_operatorController, Button.kB.value);
-                final JoystickButton setTargetLowButton = new JoystickButton(m_operatorController, Button.kA.value);
-                final JoystickButton cargoLoadButton = new JoystickButton(m_operatorController,
-                                Button.kLeftBumper.value);
+  private void configureButtonBindings() {
+    // Create some buttons
+    final JoystickButton shootButton = new JoystickButton(m_operatorController, Button.kRightBumper.value);
+    final JoystickButton shooterOffButton = new JoystickButton(m_operatorController, Button.kX.value);
+    final JoystickButton setTargetHighNearButton = new JoystickButton(m_operatorController,
+        Button.kY.value);
+    final JoystickButton setTargetHighFarButton = new JoystickButton(m_operatorController, Button.kB.value);
+    final JoystickButton setTargetLowButton = new JoystickButton(m_operatorController, Button.kA.value);
+    final JoystickButton cargoLoadButton = new JoystickButton(m_operatorController,
+        Button.kLeftBumper.value);
 
-                // TODO: Figure out how to use triggers
-                // https: //
-                // github.com/Team319/frc319-2019/blob/master/src/main/java/org/usfirst/frc/team319/controllers/BobAxisButton.java
+    // TODO: Figure out how to use triggers
+    // https: //
+    // github.com/Team319/frc319-2019/blob/master/src/main/java/org/usfirst/frc/team319/controllers/BobAxisButton.java
 
-                final JoystickButton cargoEjectButton = new JoystickButton(m_operatorController,
-                                Button.kBack.value);
+    final JoystickButton cargoEjectButton = new JoystickButton(m_operatorController,
+        Button.kBack.value);
 
-                // TODO. How do we map DPAD buttons??
-                // final JoystickButton dpadUp = new JoystickButton(m_operatorController, 5);
-                // final JoystickButton dpadRight = new JoystickButton(m_operatorController, 6);
-                // final JoystickButton dpadDown = new JoystickButton(m_operatorController, 7);
-                // final JoystickButton dpadLeft = new JoystickButton(m_operatorController, 8);
+    // TODO. How do we map DPAD buttons??
+    // final JoystickButton dpadUp = new JoystickButton(m_operatorController, 5);
+    // final JoystickButton dpadRight = new JoystickButton(m_operatorController, 6);
+    // final JoystickButton dpadDown = new JoystickButton(m_operatorController, 7);
+    // final JoystickButton dpadLeft = new JoystickButton(m_operatorController, 8);
 
-                /**
-                 * SET SHOOTING TARGET
-                 * Setting the shooting target will update the shooter motor setpoint
-                 */
+    /**
+     * SET SHOOTING TARGET
+     * Setting the shooting target will update the shooter motor setpoint
+     */
+    setTargetHighNearButton.whenPressed(
+        new InstantCommand(() -> setShootingMode(OIConstants.kTargetHighNear)));
+    setTargetHighFarButton.whenPressed(
+        new InstantCommand(() -> setShootingMode(OIConstants.kTargetHighFar)));
+    setTargetLowButton.whenPressed(
+        new InstantCommand(() -> setShootingMode(OIConstants.kTargetLow)));
 
-                setTargetHighNearButton.whenPressed(
-                                new InstantCommand(() -> setShootingMode(OIConstants.kTargetHighNear)));
-                setTargetHighFarButton.whenPressed(
-                                new InstantCommand(() -> setShootingMode(OIConstants.kTargetHighFar)));
-                setTargetLowButton.whenPressed(
-                                new InstantCommand(() -> setShootingMode(OIConstants.kTargetLow)));
+    /**
+     * SHOOT ACTION
+     * Shooting will run the feeder, but only if the shooter is up to speed.
+     * The speed depends on which shooting target mode we are in.
+     * Releasing the shoot trigger will stop the feeder.
+     */
 
-                /**
-                 * SHOOT ACTION
-                 * Shooting will run the feeder, but only if the shooter is up to speed.
-                 * The speed depends on which shooting target mode we are in.
-                 * Releasing the shoot trigger will stop the feeder.
-                 */
+    // TODO: Set time to automatically turn off the shooter motor
+    // See Timer.getFPGATimestamp
 
-                // TODO: Set time to automatically turn off the shooter motor
-                // See Timer.getFPGATimestamp
+    shootButton.whenPressed(
+        new Shoot(m_intake, m_feeder, m_shooter, m_conveyor))
+        .whenReleased(new InstantCommand(m_feeder::stopFeeder, m_feeder)
+            .alongWith(new InstantCommand(m_conveyor::stopConveyor, m_conveyor)));
 
-                shootButton.whenPressed(
-                                new Shoot(m_intake, m_feeder, m_shooter, m_conveyor))
-                                .whenReleased(new InstantCommand(m_feeder::stopFeeder, m_feeder)
-                                                .alongWith(new InstantCommand(m_conveyor::stopConveyor, m_conveyor)));
+    shooterOffButton.whenPressed(new InstantCommand(m_shooter::stopShooter, m_shooter)
+        .andThen(() -> setShootingMode(OIConstants.kShooterOff)));
 
-                shooterOffButton.whenPressed(new InstantCommand(m_shooter::stopShooter, m_shooter)
-                                .andThen(() -> setShootingMode(OIConstants.kShooterOff)));
+    /**
+     * LOAD CARGO
+     * Open the intake hatch when you run intake
+     * When you release the cargo load button, hold the cargo by stopping all motors
+     * Close the intake hatch after 2 seconds without loading cargo
+     */
 
-                /**
-                 * LOAD CARGO
-                 * Open the intake hatch when you run intake
-                 * When you release the cargo load, hold the cargo by stopping all motors
-                 * Close the intake hatch after N seconds without loading cargo
-                 */
+    cargoLoadButton.whenPressed(new LoadCargo(m_intake, m_hatch, m_conveyor, m_feeder, m_shooter))
+        .whenReleased(new HoldCargo(m_intake, m_conveyor, m_feeder)
+            .andThen(new WaitCommand(2)
+                .andThen(new InstantCommand(m_hatch::closeIntake,
+                    m_hatch))));
 
-                cargoLoadButton.whenPressed(new LoadCargo(m_intake, m_hatch, m_conveyor, m_feeder, m_shooter))
-                                .whenReleased(new HoldCargo(m_intake, m_conveyor, m_feeder)
-                                                .andThen(new WaitCommand(2)
-                                                                .andThen(new InstantCommand(m_hatch::closeIntake,
-                                                                                m_hatch))));
+    /**
+     * EJECT CARGO
+     * When you release cargo eject, hold any remaining cargo by stopping all motors
+     */
+    cargoEjectButton.whenPressed(new EjectCargo(m_intake, m_conveyor, m_feeder))
+        .whenReleased(new HoldCargo(m_intake, m_conveyor, m_feeder));
 
-                /**
-                 * EJECT CARGO
-                 * Open the intake hatch when you want to eject cargo
-                 * When you release cargo eject, hold any remaining cargo
-                 * by stopping all motors
-                 * Close the intake hatch after N seconds without ejecting cargo
-                 */
+  }
 
-                cargoEjectButton.whenPressed(new EjectCargo(m_intake, m_conveyor, m_feeder))
-                                .whenReleased(new HoldCargo(m_intake, m_conveyor, m_feeder)
-                                                .andThen(new WaitCommand(2)
-                                                                .andThen(new InstantCommand(m_hatch::closeIntake,
-                                                                                m_hatch))));
+  private void setShootingMode(int mode) {
+    shootMode = mode;
+    shootingModeDisplay.setString(shootingMode.get(shootMode));
+    m_shooter.setSetpoint(ShooterConstants.kShooterTargetRPM[mode]);
+  }
 
-        }
-
-        private void setShootingMode(int mode) {
-                shootMode = mode;
-                shootingModeDisplay.setString(shootingMode.get(shootMode));
-                m_shooter.setSetpoint(ShooterConstants.kShooterTargetRPM[mode]);
-        }
-
-        /**
-         * Use this to pass the autonomous command to the main {@link Robot}
-         * class. @return the command to run in autonomous
-         */
-        public Command getAutonomousCommand() {
-                // An ExampleCommand will run in autonomous
-                return m_autoCommand;
-        }
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
 }
