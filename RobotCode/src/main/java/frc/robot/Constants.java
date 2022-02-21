7f79870e66e487452d830fa347ae0df90bec4707
Adriana Corona <adrcoron@amazon.com>
2022-02-21 11:58:10 -0500

    Open loop ramp for drive train

--- RobotCode/src/main/java/frc/robot/Constants.java
+++ RobotCode/src/main/java/frc/robot/Constants.java
@@ -21,6 +21,7 @@ public final class Constants {
     public static final int kRightMotor2Port = 4;
     public static final int kLeftMotor1Port = 1;
     public static final int kLeftMotor2Port = 3;
+    public static final double kRampRate = 0.5;
 
     public static final int[] kRightEncoderPorts = new int[] { 2, 4 };
     public static final int[] kLeftEncoderPorts = new int[] { 1, 3 };
--- RobotCode/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ RobotCode/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -13,34 +13,36 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 public class DriveTrain extends SubsystemBase {
-        /** Creates a new DriveTrain. */
+  /** Creates a new DriveTrain. */
 
-        // Left motors
-        private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(
-                        new CANSparkMax(DriveConstants.kLeftMotor1Port,
-                                        MotorType.kBrushless),
-                        new CANSparkMax(DriveConstants.kLeftMotor2Port,
-                                        MotorType.kBrushless));
+  // Left motors
+  private final CANSparkMax left1 = new CANSparkMax(DriveConstants.kLeftMotor1Port, MotorType.kBrushless);
+  private final CANSparkMax left2 = new CANSparkMax(DriveConstants.kLeftMotor1Port, MotorType.kBrushless);
 
-        // Right motors
-        private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(
-                        new CANSparkMax(DriveConstants.kRightMotor1Port,
-                                        MotorType.kBrushless),
-                        new CANSparkMax(DriveConstants.kRightMotor2Port,
-                                        MotorType.kBrushless));
+  // Right motors
+  private final CANSparkMax right1 = new CANSparkMax(DriveConstants.kRightMotor1Port,
+      MotorType.kBrushless);
+  private final CANSparkMax right2 = new CANSparkMax(DriveConstants.kRightMotor2Port,
+      MotorType.kBrushless);
 
-        // The robot's drive
-        private final DifferentialDrive m_drive = new DifferentialDrive(
-                        m_leftMotors, m_rightMotors);
+  // Create motor groups
+  private final MotorControllerGroup m_leftMotors = new MotorControllerGroup(left1, left2);
+  private final MotorControllerGroup m_rightMotors = new MotorControllerGroup(right1, right2);
 
-        // Drive the robot using ArcadeDrive command
-        // TODO:
-        // https://codedocs.revrobotics.com/java/com/revrobotics/cansparkmax#setOpenLoopRampRate(double
-        public void ArcadeDrive(double speed, double rotation) {
-                m_drive.arcadeDrive(-1 * (Math.pow(speed, 3) * .5), -rotation * .5);
-        }
+  // The robot's drive
+  private final DifferentialDrive m_drive = new DifferentialDrive(
+      m_leftMotors, m_rightMotors);
 
-        public DriveTrain() {
-                m_leftMotors.setInverted(true);
-        }
+  // Drive the robot using ArcadeDrive command
+  public void ArcadeDrive(double speed, double rotation) {
+    m_drive.arcadeDrive(-1 * (Math.pow(speed, 3)), -rotation);
+  }
+
+  public DriveTrain() {
+    m_leftMotors.setInverted(true);
+    left1.setOpenLoopRampRate(DriveConstants.kRampRate);
+    left2.setOpenLoopRampRate(DriveConstants.kRampRate);
+    right1.setOpenLoopRampRate(DriveConstants.kRampRate);
+    right2.setOpenLoopRampRate(DriveConstants.kRampRate);
+  }
 }
