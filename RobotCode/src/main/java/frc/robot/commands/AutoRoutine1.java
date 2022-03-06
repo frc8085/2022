50c30c4217853caf2bdcb396164511e7bd596223
adrianacorona <adriana.corona@gmail.com>
2022-03-05 23:12:29 -0500

    Wait before driving

--- RobotCode/src/main/java/frc/robot/commands/AutoRoutine1.java
+++ RobotCode/src/main/java/frc/robot/commands/AutoRoutine1.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.GTADrive;
@@ -24,6 +25,7 @@ public class AutoRoutine1 extends SequentialCommandGroup {
       Intake intake) {
     addCommands(
         new ShootLowNear(intake, feeder, shooter, conveyor)
+            .andThen(new WaitCommand(1))
             .andThen(new DriveStraight(24, drive)));
   }
 }
