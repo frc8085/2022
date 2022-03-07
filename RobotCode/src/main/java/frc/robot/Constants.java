b0219fd4c44aced390fcbac953bfe147fba3f67a
lseluga <loriann.higashi@yale.edu>
2022-03-07 15:31:53 -0500

    reordering shooter constants from high to low

--- RobotCode/src/main/java/frc/robot/Constants.java
+++ RobotCode/src/main/java/frc/robot/Constants.java
@@ -98,14 +98,14 @@ public final class Constants {
     public static final int kShooterOffSpeed = 0;
 
     // High shot speeds
-    public static final int kHighNearSpeed = -2100;
     public static final int kHighFarSpeed = -3800;
     public static final int kHighAngledSpeed = -3500;
+    public static final int kHighNearSpeed = -2100;
 
     // Low shot speeds
-    public static final int kLowNearSpeed = -1800;
     public static final int kLowFarSpeed = -4000;
     public static final int kLowAngledSpeed = -2000;
+    public static final int kLowNearSpeed = -1800;
 
     public static final int[] kShooterTargetRPM = new int[] {
         kShooterOffSpeed, // 0, kShooterOff
