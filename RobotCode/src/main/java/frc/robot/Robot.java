fb5f05e6bf4f021c61db2cdfb4d3c53e1f102a79
adrianacorona <adriana.corona@gmail.com>
2022-03-08 23:09:41 -0500

    Don't load camera in simulation mode

--- RobotCode/src/main/java/frc/robot/Robot.java
+++ RobotCode/src/main/java/frc/robot/Robot.java
@@ -34,7 +34,10 @@ public class Robot extends TimedRobot {
         // Instantiate our RobotContainer. This will perform all our button bindings,
         // and put our autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
-        CameraServer.startAutomaticCapture();
+
+        if (Robot.isReal()) {
+            CameraServer.startAutomaticCapture();
+        }
     }
 
     /**
