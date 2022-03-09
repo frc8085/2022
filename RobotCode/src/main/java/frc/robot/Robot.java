ea1128a366213ba35e56846310a1ac4ee92791db
adrianacorona <adriana.corona@gmail.com>
2022-03-08 22:39:31 -0500

    Simple camera setup

--- RobotCode/src/main/java/frc/robot/Robot.java
+++ RobotCode/src/main/java/frc/robot/Robot.java
@@ -8,21 +8,14 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-import org.opencv.core.Mat;
-
 // Camera server
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.AxisCamera;
-import edu.wpi.first.cscore.CvSink;
-import edu.wpi.first.cscore.CvSource;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -34,74 +27,31 @@ public class Robot extends TimedRobot {
 
     /**
      * This function is run when the robot is first started up and should be used
-     * for any
-     * initialization code.
+     * for anyinitialization code.
      */
     @Override
     public void robotInit() {
         // Instantiate our RobotContainer. This will perform all our button bindings,
-        // and put our
-        // autonomous chooser on the dashboard.
+        // and put our autonomous chooser on the dashboard.
         m_robotContainer = new RobotContainer();
-
-        m_visionThread = new Thread(
-                () -> {
-                    // Get the Axis camera from CameraServer
-                    AxisCamera camera = CameraServer.addAxisCamera("axis-camera.local");
-
-                    // Set the resolution
-                    camera.setResolution(640, 480);
-
-                    // Get a CvSink. This will capture Mats from the camera
-                    CvSink cvSink = CameraServer.getVideo();
-                    // Setup a CvSource. This will send images back to the Dashboard
-                    CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
-
-                    // Mats are very memory expensive. Lets reuse this Mat.
-                    Mat mat = new Mat();
-
-                    // This cannot be 'true'. The program will never exit if it is. This
-                    // lets the robot stop this thread when restarting robot code or
-                    // deploying.
-                    while (!Thread.interrupted()) {
-                        // Tell the CvSink to grab a frame from the camera and put it
-                        // in the source mat. If there is an error notify the output.
-                        if (cvSink.grabFrame(mat) == 0) {
-                            // Send the output the error.
-                            outputStream.notifyError(cvSink.getError());
-                            // skip the rest of the current iteration
-                            continue;
-                        }
-
-                        // Give the output stream a new image to display
-                        outputStream.putFrame(mat);
-                    }
-                });
-        m_visionThread.setDaemon(true);
-        m_visionThread.start();
-
+        CameraServer.startAutomaticCapture();
     }
 
     /**
      * This function is called every robot packet, no matter the mode. Use this for
-     * items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and
-     * test.
+     * items like * diagnostics that you want ran during disabled, autonomous,
+     * teleoperated and test.
      *
-     * <p>
      * This runs after the mode specific periodic functions, but before LiveWindow
-     * and
-     * SmartDashboard integrated updating.
+     * and SmartDashboard integrated updating.
      */
     @Override
     public void robotPeriodic() {
         // Runs the Scheduler. This is responsible for polling buttons, adding
-        // newly-scheduled
-        // commands, running already-scheduled commands, removing finished or
-        // interrupted commands,
-        // and running subsystem periodic() methods. This must be called from the
-        // robot's periodic
-        // block in order for anything in the Command-based framework to work.
+        // newly-scheduled commands, running already-scheduled commands, removing
+        // finished or interrupted commands, and running subsystem periodic() methods.
+        // This must be called from the robot's periodic block in order for anything in
+        // the Command-based framework to work.
         CommandScheduler.getInstance().run();
     }
 
