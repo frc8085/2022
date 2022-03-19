539bd8dddba9b062cd4b03230b0b7ac1f41b7d4a
Adriana Corona <adrcoron@amazon.com>
2022-03-19 17:43:19 -0400

    Start limit switch for climber. Implements #48.

--- RobotCode/src/main/java/frc/robot/subsystems/Climber.java
+++ RobotCode/src/main/java/frc/robot/subsystems/Climber.java
@@ -7,12 +7,15 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.ClimberConstants.*;
 
 public class Climber extends SubsystemBase {
     private XboxController m_operatorController;
+    DigitalInput toplimitSwitch = new DigitalInput(0);
+    DigitalInput bottomlimitSwitch = new DigitalInput(1);
 
     // Climber is locked by default
     private boolean isLocked = true;
@@ -40,20 +43,28 @@ public class Climber extends SubsystemBase {
 
     /*
      * Will run the climber as long as it's unlocked
+     * and we haven't hit the limit switch
      */
     public void climb() {
         double rightY = m_operatorController.getRightY();
 
-        // Only climb if unlocked
-        if (isLocked) {
+        // Reasons for blocking climber:
+        // 1. Either it is locked
+        // 2. Or the top limit switch is tripped
+        // 3. Or the bottom limit switch is tripped
+        boolean blockClimb = isLocked || toplimitSwitch.get() || bottomlimitSwitch.get();
+
+        // Only climb if not blocked
+        if (blockClimb) {
             stopClimb();
         } else {
-            // deadband to remove drift when climbing
-            if (Math.abs(rightY) < .1) {
+            // Deadband to remove drift when climbing
+            if (Math.abs(rightY) < 0.1) {
                 stopClimb();
             } else {
                 m_climberMotor.set(rightY * 0.8);
             }
+
         }
     }
 
