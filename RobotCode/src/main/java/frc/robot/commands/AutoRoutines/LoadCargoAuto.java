59ff4371afad359f23753586ce7e7aa8377c08bf
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2022-04-06 19:49:27 -0400

    Revert "remove unused function"
    This reverts commit a9e6e9a1436d4de98081fe247b48f64a8d0f58d6.

---
+++ RobotCode/src/main/java/frc/robot/commands/AutoRoutines/LoadCargoAuto.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.AutoRoutines;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.HoldCargo;
+import frc.robot.commands.LoadCargo;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.IntakeCover;
+import frc.robot.subsystems.Shooter;
+
+// Load cargo in Auto. This makes sure it runs for a min. amount of seconds
+public class LoadCargoAuto extends SequentialCommandGroup {
+    private double loadCargoSecs = 0.5;
+
+    public LoadCargoAuto(
+            Intake intake,
+            Conveyor conveyor,
+            Feeder feeder,
+            Shooter shooter,
+            IntakeCover intakeCover) {
+
+        addCommands(
+                new LoadCargo(intake, intakeCover, conveyor, feeder, shooter),
+                new WaitCommand(loadCargoSecs),
+                new HoldCargo(intake, conveyor, feeder));
+    }
+}
