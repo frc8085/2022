213deaff673c83d411d0f6b1d9ee3a051235d96a
adrianacorona <adriana.corona@gmail.com>
2022-04-07 05:38:29 -0400

    Turn off autonomous when we disable

--- RobotCode/src/main/java/frc/robot/Robot.java
+++ RobotCode/src/main/java/frc/robot/Robot.java
@@ -80,7 +80,9 @@ public class Robot extends TimedRobot {
     /** This function is called once each time the robot enters Disabled mode. */
     @Override
     public void disabledInit() {
-        // m_shooter.stopShooter();
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.cancel();
+        }
     }
 
     @Override
