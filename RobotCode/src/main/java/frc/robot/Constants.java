e5fd129aa2082ed693aaf6e290e4820c74ed0c11
frc-8085 <61363993+frc-8085@users.noreply.github.com>
2022-04-10 10:43:52 -0400

    Use the same setpoints for most of the manual shots

--- RobotCode/src/main/java/frc/robot/Constants.java
+++ RobotCode/src/main/java/frc/robot/Constants.java
@@ -142,15 +142,15 @@ public final class Constants {
         public static final int kShooterOffSpeed = 0;
 
         // High shot speeds
-        public static final int kFarSpeed = -3550;
-        public static final int kTBDSpeed = -2800;
+        public static final int kFarSpeed = -3500;
+        public static final int kTBDSpeed = -1900;
         public static final int kNearSpeed = -1900;
 
         // Low shot speeds
         // public static final int kBumpedFarSpeed = -4000;
-        public static final int kBumpedFarSpeed = -2500;
-        public static final int kBumpedTBDSpeed = -2500;
-        public static final int kBumpedNearSpeed = -2500;
+        public static final int kBumpedFarSpeed = -3500;
+        public static final int kBumpedTBDSpeed = -1900;
+        public static final int kBumpedNearSpeed = -1900;
 
         // High shot delays
         public static final double kShooterOffDelay = 0;
